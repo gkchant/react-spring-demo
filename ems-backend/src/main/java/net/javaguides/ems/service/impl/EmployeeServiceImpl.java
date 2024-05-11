5638e4a1c5bdacc485ecc188851b0d6423c85445
gkchant <gkchant@gmail.com>
2024-05-10 21:45:54 -0500

    Add files via upload
    node_modules are not installed, need to install them

---
+++ ems-backend/src/main/java/net/javaguides/ems/service/impl/EmployeeServiceImpl.java
@@ -0,0 +1,59 @@
+package net.javaguides.ems.service.impl;
+
+import lombok.AllArgsConstructor;
+import net.javaguides.ems.dto.EmployeeDto;
+import net.javaguides.ems.entity.Employee;
+import net.javaguides.ems.exception.ResourceNotFoundExpection;
+import net.javaguides.ems.mapper.EmployeeMapper;
+import net.javaguides.ems.repository.EmployeeRepository;
+import net.javaguides.ems.service.EmployeeService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+@AllArgsConstructor
+public class EmployeeServiceImpl implements EmployeeService{
+    private EmployeeRepository employeeRepository;
+
+    @Override
+    public EmployeeDto createEmployee(EmployeeDto employeeDto) {
+        Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
+        Employee saveEmployee = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(saveEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeByID(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundExpection("Employee Not Found : " + employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
+    }
+
+    @Override
+    public List<EmployeeDto> getAllEmployees() {
+        List<Employee> employees = employeeRepository.findAll();
+        return employees.stream().map((employee) -> EmployeeMapper.mapToEmployeeDto(employee))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public EmployeeDto updateEmployee(Long employeeId, EmployeeDto updateEmployee) {
+         Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundExpection("Employee Not Found : " + employeeId));
+         employee.setFirstName(updateEmployee.getFirstName());
+         employee.setLastName(updateEmployee.getLastName());
+         employee.setEmail(updateEmployee.getEmail());
+         Employee updatedEmployeeObj = employeeRepository.save(employee);
+        return EmployeeMapper.mapToEmployeeDto(updatedEmployeeObj);
+    }
+
+    @Override
+    public void deleteEmployee(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()-> new ResourceNotFoundExpection("Employee Not Found : " + employeeId));
+
+        employeeRepository.deleteById(employeeId);
+    }
+}
---
+++ src/components/FooterComponent.jsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+
+const FooterComponent = () => {
+  return (
+    <div>
+        <footer className='footer'>
+            <span>All Rights reserved 2024</span>
+        </footer>
+    </div>
+  )
+}
+
+export default FooterComponent
